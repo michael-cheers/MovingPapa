2596735950b30d84efe4a85873e5ec24bf0dceec
Michael Cheers <michaelcheers@users.noreply.github.com>
2024-09-16 13:16:46 -0400

    Made needs packing help change the quote

--- MovingPapa/Pages/calculate-price.cshtml.cs
+++ MovingPapa/Pages/calculate-price.cshtml.cs
@@ -89,114 +89,123 @@ namespace MovingPapa.Pages
             var q = await DB.QuotesAndContacts.SingleAsync/**/(q => q.Uuid == uuid);
             q.MoveInfo = moveDetails;
             MoveDetails moveDetailsParsed = JsonSerializer.Deserialize<MoveDetails>(moveDetails);
-            var dt = TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(moveDetailsParsed.moveDate, CultureInfo.InvariantCulture)
-                .AddHours(moveDetailsParsed.moveTime switch
-                {
-                    MoveTime.EarlyMorning => 8,
-                    MoveTime.Afternoon => 12,
-                    MoveTime.LateAfternoon => 16,
-                    MoveTime.Evening => 20
-                }), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
-            (int m, decimal secs) = GetRouteDetails(
-                moveDetailsParsed.points.Select(p => p.address).Prepend(officeAddr).Append(officeAddr).ToArray(),
-                dt
-            );
-            int numTotalBedrooms = moveDetailsParsed.rooms.Count(r => r.room == Room.Bedroom);
-            int numRelevantBedrooms = moveDetailsParsed.rooms.Count(r => getBedroomWeight(r) >= 310 && r.room == Room.Bedroom);
-            int numRelevantExtraRooms = moveDetailsParsed.rooms.Count(r => r.items.Length > 0 &&
-                r.room is Room.Backyard or Room.Garage or Room.Basement or Room.StorageLocker or Room.Other);
-            int movers = (numRelevantBedrooms, moveDetailsParsed.points[0].buildingType) switch
+            bool needsPackingHelp = true;
+            List<decimal> prices = new();
+            do
             {
-                ( 3, buildingType: BuildingType.Apartment or BuildingType.Studio) => numRelevantBedrooms,
-                ( >= 4, _) => 4 + (numRelevantBedrooms - 3) / 2,
-                var p => numRelevantBedrooms + 1
-            } + (numRelevantExtraRooms > 0 ? 1 : 0);
-            decimal dayRate = ((await DB.RateCalendars.SingleOrDefaultAsync(r => r.Date == DateTime.Parse(moveDetailsParsed.moveDate, CultureInfo.InvariantCulture)))?.RatePerMoverInCents ?? 6000) / 100m;
-            decimal pricePerHour = movers * dayRate;
-            int numBoxes = moveDetailsParsed.rooms.Sum(r => r.items.Where(i => i.item == "Boxes").Sum(i => i.quantity));
-            decimal hours = ((decimal)secs / 3600)
-                + moveDetailsParsed.points[0].buildingType
-                switch
+                needsPackingHelp = !needsPackingHelp;
+                var dt = TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(moveDetailsParsed.moveDate, CultureInfo.InvariantCulture)
+                    .AddHours(moveDetailsParsed.moveTime switch
+                    {
+                        MoveTime.EarlyMorning => 8,
+                        MoveTime.Afternoon => 12,
+                        MoveTime.LateAfternoon => 16,
+                        MoveTime.Evening => 20
+                    }), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"));
+                (int m, decimal secs) = GetRouteDetails(
+                    moveDetailsParsed.points.Select(p => p.address).Prepend(officeAddr).Append(officeAddr).ToArray(),
+                    dt
+                );
+                int numTotalBedrooms = moveDetailsParsed.rooms.Count(r => r.room == Room.Bedroom);
+                int numRelevantBedrooms = moveDetailsParsed.rooms.Count(r => getBedroomWeight(r) >= 310 && r.room == Room.Bedroom);
+                int numRelevantExtraRooms = moveDetailsParsed.rooms.Count(r => r.items.Length > 0 &&
+                    r.room is Room.Backyard or Room.Garage or Room.Basement or Room.StorageLocker or Room.Other);
+                int movers = (numRelevantBedrooms, moveDetailsParsed.points[0].buildingType) switch
                 {
-                    BuildingType.House => 5m,
-                    BuildingType.Apartment => 10m
-                } * ((numBoxes - 1) / 3) / movers
-                + moveDetailsParsed.rooms.Sum(r => r.room switch
-                {
-                    Room.LivingRoom => numTotalBedrooms switch
+                    (3, buildingType: BuildingType.Apartment or BuildingType.Studio) => numRelevantBedrooms,
+                    ( >= 4, _) => 4 + (numRelevantBedrooms - 3) / 2,
+                    var p => numRelevantBedrooms + 1
+                } + (numRelevantExtraRooms > 0 ? 1 : 0);
+                decimal dayRate = ((await DB.RateCalendars.SingleOrDefaultAsync(r => r.Date == DateTime.Parse(moveDetailsParsed.moveDate, CultureInfo.InvariantCulture)))?.RatePerMoverInCents ?? 6000) / 100m;
+                decimal pricePerHour = movers * dayRate;
+                int numBoxes = moveDetailsParsed.rooms.Sum(r => r.items.Where(i => i.item == "Boxes").Sum(i => i.quantity));
+                decimal hours = ((decimal)secs / 3600)
+                    + moveDetailsParsed.points[0].buildingType
+                    switch
                     {
-                        1 or 2 => 75m / 60,
-                        3 or 4 => 90m / 60
-                    } * r.items.Sum(i => i.quantity * i.item switch
+                        BuildingType.House => 5m,
+                        BuildingType.Apartment => 10m
+                    } * ((numBoxes - 1) / 3) / movers
+                    + moveDetailsParsed.rooms.Sum(r => r.room switch
                     {
-                        "Couch / Sofa" => 200,
-                        "Coffee table" => 50,
-                        "TV stand" => 50,
-                        "TV" => 30,
-                        "Bookshelf" => 75,
-                        "Rug" => 50,
-                        "Side table" => 25,
-                        "Plant" => 25,
-                        "Artwork" => 5,
-                        "Extras" => 25,
-                        "Boxes" => 0
-                    }) / 535m * 1.05m,
-                    Room.Bedroom => (moveDetailsParsed.needsPackingHelp ? 0.5m : 0m) + numTotalBedrooms switch { 1 or 2 => 1m, 3 or 4 => 1.25m } *
-                        getBedroomWeight(r) / 620 * 1.05m,
-                    Room.Kitchen => movers switch
-                    {
-                        2 => 0.5m,
-                        >=3 => 20m / 60
-                    } * r.items.Sum(i => i.quantity * i.item switch
-                    {
-                        "Refrigerator" => 200,
-                        "Freezer" => 75,
-                        "Washer/dryer" => 100,
-                        "Extras" => 25,
-                        "Boxes" => 0
-                    }) / 375m * 1.05m,
-                    Room.DiningRoom => (moveDetailsParsed.needsPackingHelp ? 0.5m : 20m / 60) * r.items.Sum(i => i.quantity * i.item switch
-                    {
-                        "Cabinet" => 150,
-                        "Dining table" => 120,
-                        "Chairs" => 10,
-                        "Extras" => 25
-                    }) / 310m * 1.05m,
-                    Room.Office => (40m / 60) * r.items.Sum(i => i.quantity * i.item switch
-                    {
-                        "Desk" => 75,
-                        "Chair" => 25,
-                        "Bookshelf" => 50,
-                        "Filing cabinet" => 50,
-                        "Monitors" => 15,
-                        "Extras" => 25
-                    }) / 215m * 1.05m,
-                    _ => r.items.Sum(i => i.quantity * i.item switch
-                    {
-                        "Patio" => 15m / 60,
-                        "Furniture" => 15m / 60,
-                        "Barbecue" => 5m / 60,
-                        "Exercise equipment" => 15m / 60,
-                        "Bicycles" => 5m / 60,
-                        "Tires" => 5m / 60,
-                        "Large toys" => 5m / 60
-                    })
-                });
-            decimal km = m / 1000;
-            decimal price = Math.Round(hours * pricePerHour + km * 0.96m, 2);
-            var packages = new[]
-            {
-                new
-                {
-                    price,
-                    time = hours
-                },
-                new
-                {
-                    price = Math.Round(price * 1.5m, 2),
-                    time = hours * 1.5m
-                }
-            };
+                        Room.LivingRoom => numTotalBedrooms switch
+                        {
+                            1 or 2 => 75m / 60,
+                            3 or 4 => 90m / 60
+                        } * r.items.Sum(i => i.quantity * i.item switch
+                        {
+                            "Couch / Sofa" => 200,
+                            "Coffee table" => 50,
+                            "TV stand" => 50,
+                            "TV" => 30,
+                            "Bookshelf" => 75,
+                            "Rug" => 50,
+                            "Side table" => 25,
+                            "Plant" => 25,
+                            "Artwork" => 5,
+                            "Extras" => 25,
+                            "Boxes" => 0
+                        }) / 535m * 1.05m,
+                        Room.Bedroom => (needsPackingHelp ? 0.5m : 0m) + numTotalBedrooms switch { 1 or 2 => 1m, 3 or 4 => 1.25m } *
+                            getBedroomWeight(r) / 620 * 1.05m,
+                        Room.Kitchen => movers switch
+                        {
+                            2 => 0.5m,
+                            >= 3 => 20m / 60
+                        } * r.items.Sum(i => i.quantity * i.item switch
+                        {
+                            "Refrigerator" => 200,
+                            "Freezer" => 75,
+                            "Washer/dryer" => 100,
+                            "Extras" => 25,
+                            "Boxes" => 0
+                        }) / 375m * 1.05m,
+                        Room.DiningRoom => (needsPackingHelp ? 0.5m : 20m / 60) * r.items.Sum(i => i.quantity * i.item switch
+                        {
+                            "Cabinet" => 150,
+                            "Dining table" => 120,
+                            "Chairs" => 10,
+                            "Extras" => 25
+                        }) / 310m * 1.05m,
+                        Room.Office => (40m / 60) * r.items.Sum(i => i.quantity * i.item switch
+                        {
+                            "Desk" => 75,
+                            "Chair" => 25,
+                            "Bookshelf" => 50,
+                            "Filing cabinet" => 50,
+                            "Monitors" => 15,
+                            "Extras" => 25
+                        }) / 215m * 1.05m,
+                        _ => r.items.Sum(i => i.quantity * i.item switch
+                        {
+                            "Patio" => 15m / 60,
+                            "Furniture" => 15m / 60,
+                            "Barbecue" => 5m / 60,
+                            "Exercise equipment" => 15m / 60,
+                            "Bicycles" => 5m / 60,
+                            "Tires" => 5m / 60,
+                            "Large toys" => 5m / 60
+                        })
+                    });
+                decimal km = m / 1000;
+                decimal price = Math.Round(hours * pricePerHour + km * 0.96m, 2);
+                prices.Add(price);
+            }
+            while (needsPackingHelp == false);
+            //var packages = new[]
+            //{
+            //    new
+            //    {
+            //        price,
+            //        time = hours
+            //    },
+            //    new
+            //    {
+            //        price = Math.Round(price * 1.5m, 2),
+            //        time = hours * 1.5m
+            //    }
+            //};
+            var packages = prices.Select(price => new { price });
             q.Packages = JsonSerializer.Serialize(packages);
             q.TimeUpdated = DateTime.UtcNow;
             await DB.SaveChangesAsync();
