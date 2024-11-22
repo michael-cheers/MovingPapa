fb6d3a6020b6a3fa6afdbf789ee76e874678e02a
Michael Cheers <michaelcheers@users.noreply.github.com>
2024-11-22 15:26:43 -0500

    Moved reviews to top, added them on commercial, swapped two reviews

--- MovingPapa/Pages/rates.cshtml.cs
+++ MovingPapa/Pages/rates.cshtml.cs
@@ -20,9 +20,9 @@ namespace MovingPapa.Pages
             var revs =
                     new[]
                     {
-                        new review("Kevin and Joel couldn’t have been more amazing. They were prompt and extremely professional, they handled everything with care.", "Catherine MacFadyen", "CatherineMacFadyen.jpg"),
-                        new review("Rohit and Sam were amazing with the move. Very fast and efficient. Respectful, friendly and no damages!! Recommend Moving Papa 10/10.", "Niki V", "NikiV.jpg"),
                         new review("kevin and joel did a great job fast and cared a lot about not damaging anything", "McFury", "McFury.jpg"),
+                        new review("Rohit and Sam were amazing with the move. Very fast and efficient. Respectful, friendly and no damages!! Recommend Moving Papa 10/10.", "Niki V", "NikiV.jpg"),
+                        new review("Kevin and Joel couldn’t have been more amazing. They were prompt and extremely professional, they handled everything with care.", "Catherine MacFadyen", "CatherineMacFadyen.jpg"),
                         new review("Sam nd Rohit came did an amazing job! Moving papa is really a great company to deal with! From booking to offload these guys are amazingly smooth! I’m so happy I found these guys", "Eestbound Beats", "EestboundBeats.jpg"),
                         new review("I am always hesitant when selecting a moving company but i was suggested Moving Papa a lot and oh i am very glad that i chose them. They turned a very stressful day into a breeze.", "Unique Basnet", "UniqueBasnet.jpg"),
                         new review("Amazing job! Thanks Rohit and Abel did a great job. Highly recommend!", "Mary Rankowski", "MaryRankowski.jpg")
