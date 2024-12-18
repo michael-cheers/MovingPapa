89bc75488406e54fb461e7a26eafb5c900a64f16
Michael Cheers <michaelcheers@users.noreply.github.com>
2024-12-17 19:46:41 -0500

    Updated site UI

--- MovingPapa/Program.cs
+++ MovingPapa/Program.cs
@@ -39,9 +39,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseResponseCompression();
 app.UseHttpsRedirection();
-app.UseRewriter(new RewriteOptions().AddRewrite("^((residential|seniors|commercial)-moving|packing|storage)$", "index.html", true));
+app.UseRewriter(new RewriteOptions().AddRewrite("^service-area/(bradford|toronto|oshawa|scarborough|pickering|vaughan|oakville|hamilton|celadon|brampton|mississauga|milton|st-catherines|landon|kingston|burlington|markham|richmond-hill|new-market|barrie|aurora|kitchener|waterloo|vancouver|burnaby|richmond|surrey|ottawa|gatineau|saskatchewan|winnipeg|saskatoon|halifax|regina|calgary|edmonton|kelowna|victoria|vancouver-island|new-brunswick)|(((residential|seniors|commercial)-moving|packing|storage))$", "index.html", true));
 app.UseDefaultFiles();
-app.UseWebOptimizer();
+//app.UseWebOptimizer();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseForwardedHeaders();
